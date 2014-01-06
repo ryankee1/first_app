a316f18ca0b3865293322a5ad29465af2e874327
Ryan Kee <ryan@data-imagery.com>
2014-01-06 12:05:10 -0800

    Ruby test push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'c6425808565acc32843f2d41808112b6a364b0faaecd1793fd64e0afe41b4047b92ffbce236eaa3224ab03ec6d4447e99eb54d6bd0dbd4492b834b2061eb2dce'
